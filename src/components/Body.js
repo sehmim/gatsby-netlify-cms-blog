0a3ba268c4588a7b5b8f8f5caf935b50eb52eb12
Sehmim Haque <sehmim@Sehmims-MacBook-Pro.local>
2020-09-06 15:52:10 -0700

    adding wrapper

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,9 +3,10 @@ import React from 'react'
 function Body() {
     return (
         <div style={{display: "flex", flexWrap: "wrap", flexDirection: "column"}}>
-            <div style={{width: "100%", display: "flex", flexDirection: "row"}}>
-                <div style={{ boxShadow: "2.5px 5px", height: "460px", width: "100%", border: "1px solid black", margin: "2.5px"}}>SOMETHING HERE</div>
-                <div style={{  width: window.innerWidth /3, margin: "5px"}}>
+            
+            <div style={{width: "100%", display: "inline-flex", flexDirection: "row", flexWrap: "wrap"}}>
+                <div style={{ boxShadow: "2.5px 5px", height: "460px", width: "60%", border: "1px solid black", margin: "2.5px"}}>SOMETHING HERE</div>
+                <div style={{  width: window.innerWidth /5, margin: "5px"}}>
                     <div style={{border: "1px solid black", boxShadow: "2.5px 5px", height: "200px",margin: "0px 0px 15px 15px"}}>
                         Contact me
                     </div>
@@ -15,11 +16,11 @@ function Body() {
                 </div>
             </div>
             
-            <div style={{width: "100%", display: "flex", flexDirection: "row", margin: "5px 0px"}}>
+            <div style={{width: "100%", display: "inline-flex", flexDirection: "row", margin: "5px 0px"}}>
                 <div style={{ boxShadow: "2.5px 5px", height: "460px", width: "100%", border: "1px solid black", margin: "2.5px"}}>SOMETHING HERE</div>
             </div>
             
-            <div style={{width: "100%", display: "flex", flexDirection: "row"}}>
+            <div style={{width: "100%", display: "inline-flex", flexDirection: "row"}}>
                 <div style={{ boxShadow: "2.5px 5px", height: "460px", width: "100%", border: "1px solid black", margin: "5.5px"}}>SOMETHING HERE</div>
                 <div style={{ boxShadow: "2.5px 5px", height: "460px", width: "100%", border: "1px solid black", margin: "5.5px"}}>SOMETHING HERE</div>
                 <div style={{ boxShadow: "2.5px 5px", height: "460px", width: "100%", border: "1px solid black", margin: "5.5px"}}>SOMETHING HERE</div>
