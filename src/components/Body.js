c11a1a0a3988d649c84f0b2aac96a58f50eaf583
Sehmim Haque <sehmim@Sehmims-MacBook-Pro.local>
2020-09-06 22:24:16 -0700

    adding icon

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Body() {
+    const [innerWidth, setInnerWidth] = useState(window.innerWidth)
     return (
         <div style={{display: "flex", flexWrap: "wrap", flexDirection: "column"}}>
 
@@ -8,7 +9,7 @@ function Body() {
 
                 <div style={{ boxShadow: "2.5px 5px", height: "460px", width: "60%", border: "1px solid black", margin: "2.5px"}}>SOMETHING HERE</div>
 
-                <div style={{  width: window.innerWidth /5, margin: "5px"}}>
+                <div style={{  width: innerWidth /5, margin: "5px"}}>
                     <div style={{border: "1px solid black", boxShadow: "2.5px 5px", height: "200px",margin: "0px 0px 15px 15px"}}>
                         Contact me
                     </div>
